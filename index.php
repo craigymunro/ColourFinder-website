0f9d76b7a3d57c37aaebc933f99c460671214431
Craig Munro <craig@craigmunro.net>
2013-12-31 08:57:14 -0800

    More styling

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 	<script src="lib/ColourFinder/ColourFinder.js"></script>
 
 	<script src="lib/less/dist/less-1.5.0.min.js" type="text/javascript"></script>
+
+	<script src="//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+	<script>
+	  WebFont.load({
+	    google: {
+	      families: ['Roboto']
+	    }
+	  });
+	</script>
 </head>
 <body>
 	<div class="page">
@@ -15,58 +24,83 @@
 			<div>
 				<h1>ColourFinder</h1>
 				<p>Lightweight library to extract the dominant colour palette from an image. Uses Javascript and Canvas.</p>
-				<a href="https://raw.github.com/craigymunro/ColourFinder/master/ColourFinder.js">Download ColourFinder.js</a>
-				<a href="https://github.com/craigymunro/ColourFinder">View on Github</a>
+				<a href="https://raw.github.com/craigymunro/ColourFinder/master/ColourFinder.js" class="button">Download ColourFinder.js</a>
+				<a href="https://github.com/craigymunro/ColourFinder" class="button">View on Github</a>
 			</div>
 		</div>
+
+		<section class="what">
+			<div>
+				<h2>What is ColourFinder?</h2>
+				<p>
+					ColourFinder allows you to extract the dominant colours from an image.
+				</p>
+				
+				<p>
+					It works by identifying the colour of each individual pixel that makes up an image, and
+					returns a list of the colours most seen in the image.
+				</p>
+			</div>
+		</section>
+
 		
-		<? $image = $_GET["image"] ? $_GET["image"] : "http://www.craigmunro.net/ColourFinder/images/boston.gif"; ?>
-	
-		<form>
-			<label>Enter an image URL:</label>
-			<input name="image" placeholder="Select an image" value="<?=$image?>" style="width: 300px;"/>
-			<button>Go</button>
-		</form>
-		
-		<section>		
-			<h2>Examples</h2>
-			<? foreach(explode(",", "dali,great-wave,mona-lisa,test,Van-Gogh-Self-Portrait") as $key => $image) { ?>
-				<div class="example" id="canvas-<?=$key?>">
-					<div class="canvas">
-						<img src="images/<?=$image?>.jpg" id="example-<?=$key?>"/>
-					</div>
-					<div>
-						<a href="javascript:findColour(<?=$key?>);">Find palette</a>
-						<div class="blocks"></div>
-					</div>
-				</div>			
-			<? } ?>			
+		<section class="examples">
+			<div>
+				<h2>Examples</h2>
+				<? foreach(explode(",", "monet,dali,great-wave,test,testcard,grass") as $key => $image) { ?>
+					<div class="example" id="canvas-<?=$key?>">
+						<div class="frame">
+							<div class="canvas">
+								<a href="javascript:findColour(<?=$key?>);"><img src="images/<?=$image?>.jpg" id="example-<?=$key?>"/></a>
+							</div>
+							<div>
+								<a href="javascript:findColour(<?=$key?>);" class="button">Extract palette</a>
+								<div class="blocks"></div>
+								<div class="details"></div>
+							</div>
+						</div>
+					</div>			
+				<? } ?>
+			</div>
 		</section>
 		
-		<section>
-			<h2>Usage</h2>
+		<section class="usage">
 <?
 $usage = <<<USE
+<script src="//ajax.googleapis.com/ajax/libs/prototype/1.7.1/prototype.js"></script>
+<script src="ColourFinder.js"></script>
+
 <script>
+// The image we'll be finding the palette for
 var image = $("demo");
 
 // Create a new instance of ColourFinder
 var finder = new ColourFinder();
 
-// Find the dominant colour in the image. Returns a hex code, i.e: "0069ff".
+// Find the dominant colour in the image. Returns a single hex code, i.e: "0069ff".
 dominant = finder.getDominant(image);
 
 // Find the colour palette of the image. Returns an array of
-// hex codes and instances, i.e:
-// { "0069ff": 12, "0069aa": 14 }
-palette = finder.getPalette(image, 8);
+// hex codes. Returns 10 colours by default.
+// ["0069ff", "0069aa", "20ba20", etc]
+palette = finder.getPalette(image, 10);
 
-// Find the average colour of the image. Returns a hex code, i.e. "00fa30".
-average = finder.getAverage(image);	
+// Find the average colour of the image. Returns a single hex code, i.e. "00fa30".
+average = finder.getAverage(image);
 </script>
 USE;
 ?>			
-			<pre><?=htmlentities($usage);?></pre>			
+			<div>
+				<h2>Usage</h2>
+								
+				<ol>
+					<li>Include Prototype.js and ColourFinder.</li>
+					<li>
+						Call one of getDominant(), getPalette, or getAverage().
+					</li>
+				</ol>
+				<pre><?=nl2br(htmlentities($usage));?></pre>
+			</div>
 		</section>
 	</div>
 	<script>
@@ -74,12 +108,14 @@ USE;
 		{
 			var image = $("example-" + key);
 			var example = $("canvas-" + key);
-			var button = example.select("a").shift();
-			button.update("Working&hellip;");
+			var frame = example.select(".frame").shift();
+			var button = example.select(".button").shift();
 			var blocks = example.select(".blocks").shift();
-
+			var details = example.select(".details").shift();
+			
 			var finder = new ColourFinder();
-			var palette = finder.getPalette(image, 8);
+			var average = finder.getAverage(image);
+			var palette = finder.getPalette(image);
 
 			image.setStyle(
 				{
@@ -87,18 +123,22 @@ USE;
 				}
 			);
 
-			example.setStyle(
+			frame.setStyle(
 				{
-					backgroundColor: "#" + palette[0]
+					backgroundColor: "#" + average
 				}
 			);
-			
+
+			button.remove();
+						
 			for(i = 0; i < palette.length; i++)
 			{
-				new Insertion.Bottom(blocks, new Element("div", { style: "background-color: #" + palette[i] }).update("#" + palette[i]));
+				new Insertion.Bottom(blocks, new Element("div", { style: "background-color: #" + palette[i] }));
 			}
 			
-			button.remove();
+			new Insertion.Bottom(details, "<strong>Average colour:</strong> #" + average + "<br/>");
+			new Insertion.Bottom(details, "<strong>Dominant colour:</strong> #" + palette[0] + "<br/>");
+			new Insertion.Bottom(details, "<strong>Palette:</strong> #" + palette.join(", #") + ".");
 		}
 	</script>
 </body>
